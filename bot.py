3d2ff745a342d6a111da9cfa7ac9e9da0df5eb0e
ImMisterVish <89230534+ImMisterVish@users.noreply.github.com>
2021-08-20 07:10:52 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='.') # you can set anyone ex '.'/'!'/'&'/'dp!'
+
+@client.event
+async def on_ready():
+    print("Bot is online now")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+
+client.run('your_bot_token') #set your token
